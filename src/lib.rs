4c456b3fe6e4df6aad4fd5fe54728f5918b56fc4
zitudu <zitudu@protonmail.com>
2020-09-23 22:28:55 +0800

    feat: accept param

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,29 @@
 use std::time::{Instant, Duration};
 use std::mem::MaybeUninit;
 use std::rc::Rc;
+use std::marker::PhantomData;
 
-pub enum Memoized<'a, T> {
-    UnInitialized(Box<'a + FnMut() -> T>),
+pub enum Memoized<'a, I, T> {
+    UnInitialized(Box<dyn 'a + FnMut(I) -> T>),
     Data(T),
 }
 
-impl<'a, T> Memoized<'a, T> {
-    pub fn new<F: 'a + FnMut() -> T>(f: F) -> Self {
+impl<'a, I, T> Memoized<'a, I, T> {
+    pub fn new<F: 'a + FnMut(I) -> T>(f: F) -> Self {
         Self::UnInitialized(Box::new(f))
     }
 
-    pub fn get(&mut self) -> &T {
+    pub fn get(&mut self, input: I) -> &T {
         match self {
             Self::UnInitialized(ref mut f) => {
-                let t: T = f();
+                let t: T = f(input);
                 *self = Self::Data(t);
-                self.get()
+                match self {
+                    Self::Data(ref t) => {
+                        t
+                    }
+                    _ => unreachable!()
+                }
             }
             Self::Data(ref t) => {
                 t
@@ -26,15 +32,19 @@ impl<'a, T> Memoized<'a, T> {
     }
 }
 
-pub struct MemoizedWithExpiration<'a, T> {
+pub fn memoize<'a, I, T, F: 'a + FnMut(I) -> T>(f: F) -> Memoized<'a, I, T> {
+    Memoized::new(f)
+}
+
+pub struct MemoizedWithExpiration<'a, I, T> {
     duration: Duration,
     last: Instant,
-    f: Box<'a + FnMut() -> T>,
+    f: Box<'a + FnMut(I) -> T>,
     t: Option<Rc<T>>,
 }
 
-impl<'a, T> MemoizedWithExpiration<'a, T> {
-    pub fn new<F: 'a + FnMut() -> T>(f: F, duration: Duration) -> Self {
+impl<'a, I, T> MemoizedWithExpiration<'a, I, T> {
+    pub fn new<F: 'a + FnMut(I) -> T>(f: F, duration: Duration) -> Self {
         Self {
             duration,
             last: Instant::now(),
@@ -42,21 +52,17 @@ impl<'a, T> MemoizedWithExpiration<'a, T> {
             t: None,
         }
     }
-            self.last = Instant::now();
 
-    pub fn get(&mut self) -> Rc<T> {
+    pub fn get(&mut self, input: I) -> Rc<T> {
         if self.t.is_none() || self.last.elapsed() > self.duration {
-            self.t = Some(Rc::new((self.f)()));
+            self.t = Some(Rc::new((self.f)(input)));
+            self.last = Instant::now();
         }
         Rc::clone(self.t.as_ref().unwrap())
     }
 }
 
-pub fn memoize<'a, T, F: 'a + FnMut() -> T>(f: F) -> Memoized<'a, T> {
-    Memoized::new(f)
-}
-
-pub fn memoize_with_expiration<'a, T, F: 'a + FnMut() -> T>(f: F, duration: Duration) -> MemoizedWithExpiration<'a, T> {
+pub fn memoize_with_expiration<'a, I, T, F: 'a + FnMut(I) -> T>(f: F, duration: Duration) -> MemoizedWithExpiration<'a, I, T> {
     MemoizedWithExpiration::new(f, duration)
 }
 
@@ -64,31 +70,68 @@ pub fn memoize_with_expiration<'a, T, F: 'a + FnMut() -> T>(f: F, duration: Dura
 mod tests {
     use super::*;
     use std::thread::sleep;
+    use std::ops::Shl;
 
     #[test]
     fn test_memoize() {
         let mut called = 0;
-        let mut m = memoize(move || {
+        let mut m = memoize(move |()| {
             called += 1;
             called
         });
-        assert_eq!(m.get(), &1);
-        assert_eq!(m.get(), &1);
+        assert_eq!(m.get(()), &1);
+        assert_eq!(m.get(()), &1);
     }
 
     #[test]
     fn test_memoize_with_expiration() {
         let mut called = 0;
-        let mut m = memoize_with_expiration(move || {
+        let mut m = memoize_with_expiration(move |()| {
             called += 1;
             called
         }, Duration::from_secs(1));
         for _ in 0..1000 {
-            assert_eq!(m.get(), Rc::new(1));
+            assert_eq!(m.get(()), Rc::new(1));
         }
         sleep(Duration::from_secs(1));
         for _ in 0..1000 {
-            assert_eq!(m.get(), Rc::new(2));
+            assert_eq!(m.get(()), Rc::new(2));
         }
+    }
+
+    #[test]
+    fn test_memoize_in_struct() {
+        struct M<'a> {
+            d: i32,
+            m: Memoized<'a, Box<i32>, i32>,
+        }
+        let mut m = M {
+            d: 0,
+            m: memoize(|d: Box<i32>| {
+                *d + 1
+            }),
+        };
+        assert_eq!(m.m.get(Box::new(m.d)), &1);
+        m.d = 10;
+        assert_eq!(m.m.get(Box::new(m.d)), &1);
+    }
+
+    #[test]
+    fn test_memoize_with_expiration_in_struct() {
+        struct M<'a> {
+            d: i32,
+            m: MemoizedWithExpiration<'a, Box<i32>, i32>,
+        }
+        let mut m = M {
+            d: 0,
+            m: memoize_with_expiration(|d: Box<i32>| {
+                *d + 1
+            }, Duration::from_secs(1)),
+        };
+        assert_eq!(m.m.get(Box::new(m.d)), Rc::new(1));
+        m.d = 10;
+        assert_eq!(m.m.get(Box::new(m.d)), Rc::new(1));
+        sleep(Duration::from_secs(1));
+        assert_eq!(m.m.get(Box::new(m.d)), Rc::new(11));
     }
 }
